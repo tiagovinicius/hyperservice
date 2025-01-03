931c55fc3bc9a8ec3eaeaee545ab195b4f1b00e8
Tiago <tiagovinicius@gmail.com>
2025-01-03 04:03:55 +0000

    setup transparent proxy

--- apps/node-service-b/app.js
+++ apps/node-service-b/app.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
 const PORT = process.env.PORT || 3002;
 
-app.get('/fetch-data', async (req, res) => {
-    try {
-        const response = await axios.get('http://service-a.mesh:3001/data');
-        res.json({
-            message: 'Data fetched from Service A',
-            data: response.data,
-        });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: 'Failed to fetch data from Service A' });
-    }
+app.get("/fetch-data", async (req, res) => {
+  try {
+    const response = await axios.get("http://service-a.mesh/data");
+    console.log("TEM CACHE AQUI COM CERTEZA");
+    res.json({
+      message: "Data fetched from Service A",
+      data: response.data,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Failed to fetch data from Service A" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Service B is running on port ${PORT}`);
+  console.log(`Service B is running on port ${PORT}`);
 });
--- apps/node-service-b/entrypoint.sh
+++ apps/node-service-b/entrypoint.sh
@@ -3,7 +3,8 @@ echo "Installing dependencies"
 npm install
 
 echo "Setting up dataplane"
-CONTROL_PLANE_IP=172.19.0.5
+DATA_PLANE_NAME=service-b
+CONTROL_PLANE_IP=172.19.0.4
 CONTROL_PLANE_NAME=control-plane
 CONTROL_PLANE_ADMIN_USER_TOKEN=$(cat /run/secrets/control-plane-admin-user-token)
 export CONTAINER_IP=$(hostname -i)
@@ -12,14 +13,18 @@ kumactl config control-planes add \
   --address=http://$CONTROL_PLANE_IP:5681 \
   --auth-type=tokens \
   --auth-conf token=${CONTROL_PLANE_ADMIN_USER_TOKEN}
-envsubst < ./dataplane.yml | kumactl apply -f -
-kumactl generate dataplane-token --tag kuma.io/service=service-b --valid-for=720h > /.token
-kuma-dp run \
-  --name service-b \
-  --mesh default \
-  --cp-address https://$CONTROL_PLANE_IP:5678 \
-  --dataplane-token-file=/.token \
-  2>&1 | tee logs/dataplane-logs.txt &
+kumactl generate dataplane-token --tag kuma.io/service=$DATA_PLANE_NAME --valid-for=720h > /.token
+useradd -u 5678 -U kuma-dp
+kumactl install transparent-proxy \
+  --kuma-dp-user kuma-dp \
+  --redirect-dns \
+  --exclude-inbound-ports 22
+runuser -u kuma-dp -- \
+  kuma-dp run \
+    --cp-address https://$CONTROL_PLANE_IP:5678 \
+    --dataplane-token-file=/.token \
+    --dataplane="$(envsubst < ./dataplane.yml)" \
+    2>&1 | tee logs/dataplane-logs.txt &
 
 echo "Starting service"
 node app.js \
