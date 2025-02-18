0f15d4a80495320b3e5153d12cfae860a7a93f98
Tiago <tiagovinicius@gmail.com>
2025-02-18 20:08:48 +0000

    fix setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,6 +4,7 @@ git config --global user.name $GIT_NAME
 git config --global user.email $GIT_EMAIL
 git config --global safe.directory '*'
 git config --global core.editor "nano"
+git config pull.rebase false
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 chown -R root:root ~/.ssh
